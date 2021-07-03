9257ef621cdd83c8cdd18c23d6ed7a9399f31a53
nithin <nithinmrajchinju@gmail.com>
2021-07-03 10:37:11 +0530

    calculator added

---
+++ script.js
@@ -0,0 +1,16 @@
+// function displayNumber(num){
+//     inpt.value+=num
+// }
+let displayNumber=(num)=>inpt.value+=num
+
+// function evaluateExpression(){
+//     inpt.value=eval(inpt.value)
+// }
+let evaluateExpression=()=>inpt.value=eval(inpt.value)
+
+let clearBox=()=>inpt.value=""
+
+// function removeNumber(){
+//     inpt.value=inpt.value.slice(0,-1)
+// }
+let removeNumber=()=>inpt.value=inpt.value.slice(0,-1)
